75db6e2eee2b02b4c0e57a15a3a2fdeb534371b3
“GabrielaLokelani” <gabrielakadzielawa@gmail.com>
2023-05-27 03:08:31 -1000

    adding address in wallet creation and cleanup

--- src/wallet.js
+++ src/wallet.js
@@ -1,14 +1,20 @@
 
+import { RIPEMD160 } from 'crypto-js';
 import { ec as EC } from 'elliptic';
-
 const ec = new EC('secp256k1');
 
 export function createWallet() {
     const keyPair = ec.genKeyPair();
     const publicKey = keyPair.getPublic('hex');
     const privateKey = keyPair.getPrivate('hex');
+    const address = RIPEMD160(publicKey)
+
+    // console.log('here is your address: ' + address);
+    // console.log('here is your publicKey: ' + publicKey);
+    // console.log('here is your privateKey: ' + privateKey);
 
     return {
+        address,
         publicKey,
         privateKey,
         keyPair
